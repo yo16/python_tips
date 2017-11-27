e0dd5f44f60ee3c6b3c6629db523dc6a33f7b1a8
yo16 <xxyo1xx@gmail.com>
2017-11-27 12:38:15 +0900

    11/27

---
+++ if/三項演算子.py
@@ -0,0 +1,11 @@
+# -*- coding : utf-8 -*-
+
+
+testN = 11
+
+isEven = True if (testN%2==0) else False
+if isEven:
+	print('even!')
+else:
+	print('odd!')
+
---
+++ if/三項演算子_実行.bat
@@ -0,0 +1,5 @@
+set PY="C:\Program Files\Python34\python.exe"
+
+%PY% O€‰‰Zq.py
+
+pause
