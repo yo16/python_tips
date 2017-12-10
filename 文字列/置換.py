1f7882afa75469bfb17c3ecbf76451b2abc5c2c5
yo16 <xxyo1xx@gmail.com>
2017-12-11 08:40:51 +0900

    12/11

--- 文字列/置換.py
+++ 文字列/置換.py
@@ -11,3 +11,11 @@ print( str.replace('a', 'A') )
 print( str.replace('a', 'A', 2) )
 # → AbcAbcabcd
 
+
+# aまたはcをxへ置換するには、
+# 正規表現で置換する必要あり。
+#print( str.replace('[ac]', 'x') )	# NG!
+import re
+print( re.sub(r'[ac]', 'x', str) )
+# → xbxxbxxbxd
+
