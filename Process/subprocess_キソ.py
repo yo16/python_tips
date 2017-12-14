5952cd4d0bedb844efaefa7d9422005d872a8e37
yo16 <xxyo1xx@gmail.com>
2017-12-14 12:52:53 +0900

    12/14

---
+++ Process/subprocess_キソ.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+# subprocess 
+# 2017 (c) y.ikeda
+
+import subprocess
+
+
+subprocess.call(['calc'])
+
+
---
+++ Process/subprocess_キソ_実行.bat
@@ -0,0 +1,5 @@
+set PY="C:\Program Files\Python36\python.exe"
+
+%PY% subprocess_ÉLÉ\.py
+
+pause
