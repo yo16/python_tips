664acb311a3a0dbd34a4caf1e0094ff523082388
yo16 <yikeda@pinot.local>
2017-12-23 23:07:31 +0900

    12/23

--- 文字列/検索_正規表現_複数回.py
+++ 文字列/検索_正規表現_複数回.py
@@ -3,11 +3,11 @@
 import re
 
 # rにしておくとバックスラッシュの文字をエスケープしなくていい
-rePattern = r"b+(c+)"
+rePattern = r"b+(c+)(.)?"
 # コンパイルしておくと、同じパターンで何度も検索するときに速くなる
 #reObj = re.compile(rePattern)
 
-str = "aaabbbbcccbbccaa"
+str = "aaabbbbcccbbcc"
 #matchObj = reObj.search(str)
 
 
@@ -17,11 +17,22 @@ str = "aaabbbbcccbbccaa"
 
 iterator = re.finditer(rePattern, str)
 for match in iterator:
-	print(match.group(0))
-	print(match.group(1))
+	#print(match.groups('x'))  # パターン内のグループのタプル(引数あり)
+	print(match.groups())  # パターン内のグループのタプル(引数なし)
+	print(match.group(0))  # 検索対象全体
+	print(match.group(1))  # 最初の丸括弧
+	print(match.group(2))  # ２つ目の丸括弧
 
 	# マッチオブジェクトから情報を取り出す関数
 	# group()	正規表現にマッチした文字列を返す。
 	# start()	マッチの開始位置を返す。
 	# end()	マッチの終了位置を返す。
 	# span()	マッチの位置 (start, end) を含むタプルを返す。
+
+# 2つ目の丸括弧の(.)?は、ヒットしないときは
+# Noneが入る。
+# match.groups()に引数を与えてやるとNoneがその文字になる。
+# 例えばmatch.groups()では
+# ('ccc', 'b')と('cc', None)だが
+# match.groups('x')では
+# ('ccc', 'b')と('cc', 'x')になる
